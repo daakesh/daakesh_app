3837f09c5d3389ff68faffc89cdd8de22d1a9614
adellahlouh <adel.lahlouh@abwaab.com>
2024-09-23 21:01:47 +0300

    update version

--- lib/src/core/injection/inj.config.dart
+++ lib/src/core/injection/inj.config.dart
@@ -9,115 +9,118 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:daakesh/src/core/shared_preferences/secure_shared_pref.dart'
-    as _i24;
+    as _i188;
 import 'package:daakesh/src/core/shared_preferences/shared_preferences.dart'
-    as _i7;
+    as _i853;
 import 'package:daakesh/src/features/authentication/data/datasource/remote_auth_datasource.dart'
-    as _i4;
+    as _i933;
 import 'package:daakesh/src/features/authentication/data/repositories/auth_repository_impl.dart'
-    as _i5;
+    as _i492;
 import 'package:daakesh/src/features/authentication/domain/use_cases/auth_use_cases.dart'
-    as _i6;
+    as _i119;
 import 'package:daakesh/src/features/home/data/datasource/remote_home_datasource.dart'
-    as _i11;
+    as _i279;
 import 'package:daakesh/src/features/home/data/repositories/home_repository_impl.dart'
-    as _i12;
+    as _i1054;
 import 'package:daakesh/src/features/home/domain/use_cases/home_use_cases.dart'
-    as _i13;
+    as _i561;
 import 'package:daakesh/src/features/my_order/data/datasource/remote_my_order_datasource.dart'
-    as _i14;
+    as _i354;
 import 'package:daakesh/src/features/my_order/data/repositories/my_order_repository_impl.dart'
-    as _i15;
+    as _i597;
 import 'package:daakesh/src/features/my_order/domain/use_cases/my_order_use_cases.dart'
-    as _i16;
+    as _i1067;
 import 'package:daakesh/src/features/my_product/data/datasource/remote_my_product_datasource.dart'
-    as _i17;
+    as _i9;
 import 'package:daakesh/src/features/my_product/data/repositories/my_product_repository_impl.dart'
-    as _i18;
+    as _i717;
 import 'package:daakesh/src/features/my_product/domain/use_cases/my_product_use_cases.dart'
-    as _i19;
+    as _i895;
 import 'package:daakesh/src/features/profile/data/datasource/remote_profile_datasource.dart'
-    as _i21;
+    as _i365;
 import 'package:daakesh/src/features/profile/data/repositories/profile_repository_impl.dart'
-    as _i22;
+    as _i865;
 import 'package:daakesh/src/features/profile/domain/use_cases/profile_use_cases.dart'
-    as _i23;
+    as _i471;
 import 'package:daakesh/src/features/swap/data/datasource/remote_swap_datasource.dart'
-    as _i25;
+    as _i526;
 import 'package:daakesh/src/features/swap/data/repositories/swap_repository_impl.dart'
-    as _i26;
+    as _i416;
 import 'package:daakesh/src/features/swap/domain/use_cases/swap_use_cases.dart'
-    as _i27;
+    as _i549;
 import 'package:daakesh/src/services/connectivity/connectivity_service.dart'
-    as _i8;
-import 'package:daakesh/src/services/network/network_service.dart' as _i20;
-import 'package:daakesh/src/services/user_service/contact_info.dart' as _i9;
-import 'package:daakesh/src/services/user_service/edit_product.dart' as _i10;
-import 'package:daakesh/src/services/user_service/user_data.dart' as _i28;
-import 'package:daakesh/src/src.export.dart' as _i3;
-import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart' as _i2;
+    as _i971;
+import 'package:daakesh/src/services/network/network_service.dart' as _i716;
+import 'package:daakesh/src/services/user_service/contact_info.dart' as _i1018;
+import 'package:daakesh/src/services/user_service/edit_product.dart' as _i803;
+import 'package:daakesh/src/services/user_service/user_data.dart' as _i198;
+import 'package:daakesh/src/src.export.dart' as _i467;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
 
 const String _dev = 'dev';
 
-extension GetItInjectableX on _i1.GetIt {
+extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
-  _i1.GetIt init({
+  _i174.GetIt init({
     String? environment,
-    _i2.EnvironmentFilter? environmentFilter,
+    _i526.EnvironmentFilter? environmentFilter,
   }) {
-    final gh = _i2.GetItHelper(
+    final gh = _i526.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
-    gh.lazySingleton<_i3.AuthDatasource>(() => _i4.RemoteAuthDatasource());
-    gh.lazySingleton<_i3.AuthRepository>(() => _i5.AuthRepositoryImpl());
-    gh.lazySingleton<_i6.AuthUseCases>(
-      () => _i6.AuthUseCasesImpl(),
+    gh.singleton<_i198.UserData>(() => _i198.UserDataImpl());
+    gh.lazySingleton<_i467.MyProductDatasource>(
+        () => _i9.RemoteMyProductDatasource());
+    gh.lazySingleton<_i467.HomeDatasource>(() => _i279.RemoteHomeDatasource());
+    gh.singleton<_i853.CacheHelper>(() => _i853.CacheHelperImpl());
+    gh.singleton<_i971.ConnectivityService>(
+        () => _i971.ConnectivityServiceImpl());
+    gh.lazySingleton<_i467.MyOrderDatasource>(
+        () => _i354.RemoteMyOrderDatasource());
+    gh.lazySingleton<_i467.SwapRepository>(() => _i416.SwapRepositoryImpl());
+    gh.lazySingleton<_i467.HomeRepository>(() => _i1054.HomeRepositoryImpl());
+    gh.lazySingleton<_i467.AuthDatasource>(() => _i933.RemoteAuthDatasource());
+    gh.lazySingleton<_i561.HomeUseCases>(
+      () => _i561.HomeUseCasesImpl(),
       registerFor: {_dev},
     );
-    gh.singleton<_i7.CacheHelper>(_i7.CacheHelperImpl());
-    gh.singleton<_i8.ConnectivityService>(_i8.ConnectivityServiceImpl());
-    gh.singleton<_i9.ContactInfoService>(_i9.ContactInfoImpl());
-    gh.singleton<_i10.EditProduct>(_i10.EditProductImpl());
-    gh.lazySingleton<_i3.HomeDatasource>(() => _i11.RemoteHomeDatasource());
-    gh.lazySingleton<_i3.HomeRepository>(() => _i12.HomeRepositoryImpl());
-    gh.lazySingleton<_i13.HomeUseCases>(
-      () => _i13.HomeUseCasesImpl(),
+    gh.lazySingleton<_i467.SwapDatasource>(() => _i526.RemoteSwapDatasource());
+    gh.lazySingleton<_i549.SwapUseCases>(
+      () => _i549.SwapUseCasesImpl(),
       registerFor: {_dev},
     );
-    gh.lazySingleton<_i3.MyOrderDatasource>(
-        () => _i14.RemoteMyOrderDatasource());
-    gh.lazySingleton<_i3.MyOrderRepository>(() => _i15.MyOrderRepositoryImpl());
-    gh.lazySingleton<_i16.MyOrderUseCases>(
-      () => _i16.MyOrderUseCasesImpl(),
+    gh.lazySingleton<_i716.NetworkService>(() => _i716.NetworkServiceImpl());
+    gh.lazySingleton<_i471.ProfileUseCases>(
+      () => _i471.ProfileUseCasesImpl(),
       registerFor: {_dev},
     );
-    gh.lazySingleton<_i3.MyProductDatasource>(
-        () => _i17.RemoteMyProductDatasource());
-    gh.lazySingleton<_i3.MyProductRepository>(
-        () => _i18.MyProductRepositoryImpl());
-    gh.lazySingleton<_i19.MyProductUseCases>(
-      () => _i19.MyProductUseCasesImpl(),
+    gh.lazySingleton<_i467.MyProductRepository>(
+        () => _i717.MyProductRepositoryImpl());
+    gh.singleton<_i1018.ContactInfoService>(() => _i1018.ContactInfoImpl());
+    gh.lazySingleton<_i467.AuthRepository>(() => _i492.AuthRepositoryImpl());
+    gh.singleton<_i188.SecureSharedPref>(() => _i188.ImplSecureSharedPref());
+    gh.lazySingleton<_i1067.MyOrderUseCases>(
+      () => _i1067.MyOrderUseCasesImpl(),
       registerFor: {_dev},
     );
-    gh.lazySingleton<_i20.NetworkService>(() => _i20.NetworkServiceImpl());
-    gh.lazySingleton<_i3.ProfileDatasource>(
-        () => _i21.RemoteProfileDatasource());
-    gh.lazySingleton<_i3.ProfileRepository>(() => _i22.ProfileRepositoryImpl());
-    gh.lazySingleton<_i23.ProfileUseCases>(
-      () => _i23.ProfileUseCasesImpl(),
+    gh.lazySingleton<_i467.ProfileDatasource>(
+        () => _i365.RemoteProfileDatasource());
+    gh.lazySingleton<_i467.MyOrderRepository>(
+        () => _i597.MyOrderRepositoryImpl());
+    gh.lazySingleton<_i119.AuthUseCases>(
+      () => _i119.AuthUseCasesImpl(),
       registerFor: {_dev},
     );
-    gh.singleton<_i24.SecureSharedPref>(_i24.ImplSecureSharedPref());
-    gh.lazySingleton<_i3.SwapDatasource>(() => _i25.RemoteSwapDatasource());
-    gh.lazySingleton<_i3.SwapRepository>(() => _i26.SwapRepositoryImpl());
-    gh.lazySingleton<_i27.SwapUseCases>(
-      () => _i27.SwapUseCasesImpl(),
+    gh.lazySingleton<_i467.ProfileRepository>(
+        () => _i865.ProfileRepositoryImpl());
+    gh.singleton<_i803.EditProduct>(() => _i803.EditProductImpl());
+    gh.lazySingleton<_i895.MyProductUseCases>(
+      () => _i895.MyProductUseCasesImpl(),
       registerFor: {_dev},
     );
-    gh.singleton<_i28.UserData>(_i28.UserDataImpl());
     return this;
   }
 }
